9e864a9bf8862879ad89978f67d3c072d5f508b4
Nimitpamnani <C0764944@mylambton.ca>
2021-04-01 12:12:00 +0530

    message

--- TimeManger2/ViewController.swift
+++ TimeManger2/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     let sb = UIStoryboard(name: "Main", bundle: nil)
     var empType: String = ""
     
-//gnfsjk
-    
+//gnfsjkgfg
     @IBOutlet weak var empActivity: UIButton!
     
     @IBOutlet weak var empName: UILabel!
